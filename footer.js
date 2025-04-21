bb7d97bcf06e5cb389788619329aadc5090b5cab
Jean Decroocq <jdecroocq.3d@gmail.com>
2025-04-21 17:29:46 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 // footer.js
-fetch('/footer.html')
+fetch('footer.html')
   .then(response => response.text())
   .then(html => {
     document.getElementById('footer-placeholder').innerHTML = html;
