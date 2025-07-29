1b49886ebad12e904110d9595d620690b1e7665d
Jean Decroocq <jdecroocq.3d@gmail.com>
2025-07-29 19:45:11 +0200

    Create config.js
    Signed-off-by: Jean Decroocq <jdecroocq.3d@gmail.com>

---
+++ config.js
@@ -0,0 +1,7 @@
+// This API key is restricted to usage only on the portfolio URL configured in the Google Cloud Console.
+// It cannot be used elsewhere and poses no security risk.
+
+const API_KEY = 'AIzaSyDH_v9G1JUD-NEkm1bnpWlbty588Kml5Hs';
+const ROOT_FOLDER_ID = '1UzmdatXkevjNC4-D_D_kL8s8BcAS8vEC';
+
+export { API_KEY, ROOT_FOLDER_ID };
