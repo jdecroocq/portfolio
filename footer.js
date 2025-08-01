9beb87cbc8cf1fb173d8cbb7e9a04174f852ab50
Jean Decroocq <jdecroocq.3d@gmail.com>
2025-08-01 13:30:46 +0200

    Update footer.js
    Signed-off-by: Jean Decroocq <jdecroocq.3d@gmail.com>

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ fetch('/portfolio/footer.html')
   .then(response => response.text())
   .then(html => {
     document.getElementById('footer-placeholder').outerHTML = html;
-
+    document.getElementById('year').textContent = new Date().getFullYear();
     fetch('https://api.github.com/repos/jdecroocq/portfolio/releases/latest')
       .then(response => response.json())
       .then(data => {
